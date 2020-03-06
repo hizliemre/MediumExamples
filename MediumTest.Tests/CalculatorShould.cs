8cfb26c514ecd37b3865b15fb1f514120319f6ff
hizliemre@gmail.com <hizliemre@gmail.com>
2020-03-07 00:54:21 +0300

    first test

---
+++ MediumTest.Tests/CalculatorShould.cs
@@ -0,0 +1,22 @@
+﻿using Xunit;
+
+namespace MediumTest.Tests
+{
+    public class CalculatorShould
+    {
+        [Fact]
+        public void Be_able_to_add_two_numbers()
+        {
+            // Arrange
+            int number1 = 10;
+            int number2 = 20;
+            Calculator sut = new Calculator();
+
+            // Act
+            int result = sut.Sum(number1, number2);
+
+            // Assert
+            Assert.Equal(number1 + number2, result);
+        }
+    }
+}
