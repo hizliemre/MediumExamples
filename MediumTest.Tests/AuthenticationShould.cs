40b56f91afe40d7a40d6af2a3a1d53b161dd32b3
hizliemre@gmail.com <hizliemre@gmail.com>
2020-03-08 20:41:47 +0300

    memberdata righ way 2

--- MediumTest.Tests/AuthenticationShould.cs
+++ MediumTest.Tests/AuthenticationShould.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using Xunit;
 
 namespace MediumTest.Tests
@@ -48,10 +50,71 @@ namespace MediumTest.Tests
             Authentication sut = new Authentication();
 
             // Act
-            bool result = sut.Login(username);
+            bool result = sut.Login(username, password);
 
             // Assert
             Assert.True(result);
+        }
+
+        [Theory]
+        [MemberData(nameof(LoginDataFromField))]
+        public void Be_able_login_inlinedata_right_way1(string username, string password)
+        {
+            // Arrange
+            Authentication sut = new Authentication();
+
+            // Act
+            bool result = sut.Login(username, password);
+
+            // Assert
+            Assert.True(result);
+        }
+
+        public static IEnumerable<object[]> LoginDataFromField => new List<object[]>
+        {
+            new object[] {"user1", "password1" },
+            new object[] {"user2", "password2" },
+            new object[] {"user3", "password3" },
+            new object[] {"user4", "password4" },
+            new object[] {"user5", "password5" },
+            new object[] {"user6", "password6" },
+            new object[] {"user7", "password7" },
+            new object[] {"user8", "password8" },
+            new object[] {"user9", "password9" },
+            new object[] {"user10", "password10" }
+        };
+
+        [Theory]
+        [MemberData(nameof(LoginDataFromMethod), 5)]
+        public void Be_able_login_inlinedata_right_way2(string username, string password)
+        {
+            // Arrange
+            Authentication sut = new Authentication();
+
+            // Act
+            bool result = sut.Login(username, password);
+
+            // Assert
+            Assert.True(result);
+        }
+
+        public static IEnumerable<object[]> LoginDataFromMethod(int take)
+        {
+            var items = new List<object[]>
+            {
+                new object[] {"user1", "password1" },
+                new object[] {"user2", "password2" },
+                new object[] {"user3", "password3" },
+                new object[] {"user4", "password4" },
+                new object[] {"user5", "password5" },
+                new object[] {"user6", "password6" },
+                new object[] {"user7", "password7" },
+                new object[] {"user8", "password8" },
+                new object[] {"user9", "password9" },
+                new object[] {"user10", "password10" }
+            };
+
+            return items.Take(take);
         }
     }
 }
--- MediumTest/Authentication.cs
+++ MediumTest/Authentication.cs
@@ -11,5 +11,16 @@ namespace MediumTest
 
             return true;
         }
+
+        public bool Login(string username, string password)
+        {
+            if (string.IsNullOrEmpty(username) || username.Length < 4)
+                throw new InvalidOperationException();
+
+            if (string.IsNullOrEmpty(password) || password.Length < 4)
+                throw new InvalidOperationException();
+
+            return true;
+        }
     }
 }
