c54426d17479f244130741da49ca26ce70b3ea9c
hizliemre@gmail.com <hizliemre@gmail.com>
2020-03-08 19:54:37 +0300

    inline data wrong way

--- MediumTest.Tests/AuthenticationShould.cs
+++ MediumTest.Tests/AuthenticationShould.cs
@@ -34,5 +34,24 @@ namespace MediumTest.Tests
             // Assert
             Assert.Throws<InvalidOperationException>(act);
         }
+
+        [Theory]
+        [InlineData("user1")]
+        [InlineData("user2")]
+        [InlineData("user3")]
+        [InlineData("user4")]
+        [InlineData("user5"), InlineData("user6"), InlineData("user7"),
+         InlineData("user8"), InlineData("user9"), InlineData("user10")]
+        public void Be_able_login_inlinedata_wrong_way(string username)
+        {
+            // Arrange
+            Authentication sut = new Authentication();
+
+            // Act
+            bool result = sut.Login(username);
+
+            // Assert
+            Assert.True(result);
+        }
     }
 }
