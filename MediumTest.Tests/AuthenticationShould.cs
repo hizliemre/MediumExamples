5867f1dc7f9ed83b10c3f3f2e9ebd28a5e885ca5
hizliemre@gmail.com <hizliemre@gmail.com>
2020-03-08 20:05:26 +0300

    change parameter

--- MediumTest.Tests/AuthenticationShould.cs
+++ MediumTest.Tests/AuthenticationShould.cs
@@ -36,13 +36,13 @@ namespace MediumTest.Tests
         }
 
         [Theory]
-        [InlineData("user1")]
-        [InlineData("user2")]
-        [InlineData("user3")]
-        [InlineData("user4")]
-        [InlineData("user5"), InlineData("user6"), InlineData("user7"),
-         InlineData("user8"), InlineData("user9"), InlineData("user10")]
-        public void Be_able_login_inlinedata_wrong_way(string username)
+        [InlineData("user1", "password1")]
+        [InlineData("user2", "password2")]
+        [InlineData("user3", "password3")]
+        [InlineData("user4", "password4")]
+        [InlineData("user5", "password5"), InlineData("user6", "password6"), InlineData("user7", "password7"),
+         InlineData("user8", "password8"), InlineData("user9", "password9"), InlineData("user10", "password10")]
+        public void Be_able_login_inlinedata_wrong_way(string username, string password)
         {
             // Arrange
             Authentication sut = new Authentication();
