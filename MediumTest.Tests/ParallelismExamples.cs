2d0aef7c3c9a606667533211d9daa4829ebbe6b0
Emre Hızlı <hizliemre@gmail.com>
2020-03-10 12:48:37 +0300

    chapter 5

---
+++ MediumTest.Tests/ParallelismExamples.cs
@@ -0,0 +1,25 @@
+﻿using System.Threading;
+using Xunit;
+
+namespace MediumTest.Tests
+{
+    [Collection("my collection")]
+    public class ParallelismExamples1
+    {
+        [Fact]
+        public void Test1()
+        {
+            Thread.Sleep(5000);
+        }
+    }
+
+    [Collection("my collection")]
+    public class ParallelismExamples2
+    {
+        [Fact]
+        public void Test2()
+        {
+            Thread.Sleep(5000);
+        }
+    }
+}
---
+++ MediumTest.Tests/ParallelismExamplesAsync.cs
@@ -0,0 +1,20 @@
+﻿using System.Threading.Tasks;
+using Xunit;
+
+namespace MediumTest.Tests
+{
+    public class ParallelismExamplesAsync
+    {
+        [Fact]
+        public async Task Test1()
+        {
+            await Task.Delay(5000);
+        }
+
+        [Fact]
+        public async Task Test2()
+        {
+            await Task.Delay(5000);
+        }
+    }
+}
